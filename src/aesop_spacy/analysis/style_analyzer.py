9ac25fb43f602b1a31f9118c7c1ccd2bae0879b6
[Enrique Viveros] <github.villain713@slmail.me>
2025-04-18 16:50:27 +0200

    expanding style_analyzer code

--- src/aesop_spacy/analysis/style_analyzer.py
+++ src/aesop_spacy/analysis/style_analyzer.py
@@ -4,3 +4,10 @@ Compares stylistic Elements across languages by analyzing:
 * Sentence Complexity: Measure how intricate sentences are. 
 """
 
+class StyleAnalyzer:
+    def __init__(self):
+        pass
+    def sentence_complexity(self, doc):
+        """Analyze sentence complexity metrics."""
+        pass
+
